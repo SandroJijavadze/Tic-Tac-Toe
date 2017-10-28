9704a7c78a6b843bcffc18d5809067b64b375fcc
SandroJijavadze <sjijavadze@sdsu.edu>
2017-10-28 15:44:22 +0400

    gameloop 2player

--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Tic_Tac_Toe
 {
@@ -10,6 +11,17 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
+            char playagain = 'y';
+            while(playagain == 'y')
+            {
+                game = new Game();
+                game.gameLoop();
+                Console.WriteLine("Do you want to play again? y/n: ");
+                playagain = Console.ReadKey().KeyChar;
+            }
+            
+            Console.ReadLine();
         }
     }
 }
--- Tic-Tac-Toe/game.cs
+++ Tic-Tac-Toe/game.cs
@@ -11,16 +11,36 @@ using System.Threading.Tasks;
 
 namespace Tic_Tac_Toe
 {
-    class game
+    class Game
     {
-        private int[,] board = new int[3, 3]; 
-        public void makeMove(int a, int b, int c) // a = row, b = column, c = player( either 1 or 2).
+        private int[,] board = new int[3, 3];  // Initialize empty board.
+        public bool makeMove(int a, int b, bool player1) // a = row, b = column, c = player( either 1 or 2). returns -1 if move is illegal.
         {
+            int c = player1 ? 1 : 2;
             if(board[a,b] == 0)
             {
                 board[a, b] = c;
+                return true;
             }
-            return;
+            return false;
+        }
+
+        public bool move(string plmove, bool player1)
+        {
+            int row;
+            if (plmove[0] == 'A')
+                row = 0;
+            else if (plmove[0] == 'B')
+                row = 1;
+            else if (plmove[0] == 'C')
+                row = 2;
+            else
+                return false;
+            int column = (int)char.GetNumericValue(plmove[1]);
+            if (!(makeMove(row, column, player1)))
+                return false;
+            else
+                return true;
         }
         public int checkHorizontals()  //Checks if there is horizontal win, if there is, it returns row. else, returns -1.
         {
@@ -59,7 +79,51 @@ namespace Tic_Tac_Toe
                 return board[1, 1];
             return -1;
         }
-        
 
+        public void displayBoard()
+        {
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine($"{board[i, 0]} {board[i, 1]} {board[i, 2]}");
+        }
+
+        public void gameLoop()
+        {
+            char gamemode;
+            Console.WriteLine("NOTE: If you choose 2 player mode, 1st player will be chosen randomly.");
+            Console.Write("If you want to use game.txt file, enter y, if you want 2 player game, enter n:");
+            gamemode = Console.ReadKey().KeyChar;
+            if (gamemode == 'y') // Game.txt exists.
+            {
+            }
+            else // 
+            {
+                Random r = new Random();
+                bool player1 = (r.Next(1, 2) == 1)? true:false;//Choose player randomly.
+                int i = 0;
+                string playerMove = "";
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Last Move: {playerMove}");//Display last move and board.
+                    displayBoard();
+                    Console.WriteLine($"PLayer {(player1? 1:2)}, enter a move:");//Promt for move.
+                    playerMove = Console.ReadLine();
+
+                    while(!(move(playerMove, player1)))//While move  is illegal.
+                    {
+                        Console.WriteLine("Invalid Move! Enter another move:"); // Ask for new move.
+                        playerMove = Console.ReadLine();
+                    }
+                    if(checkWin() > 0)
+                    {
+                        Console.Clear();
+                        displayBoard();
+                        Console.WriteLine($"Player{(player1 ? 1 : 2)} won the game!");
+                        break;
+                    }
+                    player1 = !player1;
+                }
+            }
+        }
     }
 }
