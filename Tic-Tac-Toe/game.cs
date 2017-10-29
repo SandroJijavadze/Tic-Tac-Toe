e53030942a60ca2a00b4db00e274270ec32f8c3d
SandroJijavadze <sjijavadze@sdsu.edu>
2017-10-29 12:19:17 +0400

    Revert "play from .txt"
    This reverts commit d2651e9f0bca0cf6de9333ddab045e226a37e78a.

--- Tic-Tac-Toe/game.cs
+++ Tic-Tac-Toe/game.cs
@@ -16,17 +16,10 @@ namespace Tic_Tac_Toe
     class Game
     {
         private int[,] board = new int[3, 3];  // Initialize empty board.
-        private bool won = false;
-        private int inp;      // status of input.
-        private bool player1; // Which player is playing right now.
-        private string line;
-        private bool firstmove;
-        // IF file exists, the file will be assigned, else, it will be nulled.
-        private System.IO.StreamReader file = File.Exists(Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\")) + "game.txt") ? new System.IO.StreamReader(@Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\")) + "game.txt") : null;
-        private bool makeMove(int a, int b) // a = row, b = column, c = player( either 1 or 2). returns -1 if move is illegal.
+        private bool makeMove(int a, int b, bool player1) // a = row, b = column, c = player( either 1 or 2). returns -1 if move is illegal.
         {
             int c = player1 ? 1 : 2;
-            if (board[a, b] == 0)
+            if(board[a,b] == 0)
             {
                 board[a, b] = c;
                 return true;
@@ -34,9 +27,8 @@ namespace Tic_Tac_Toe
             return false;
         }
 
-        private bool move(string plmove)
+        private bool move(string plmove, bool player1)
         {
-            
             int row;
             if (plmove[0] == 'A')
                 row = 0;
@@ -47,7 +39,7 @@ namespace Tic_Tac_Toe
             else
                 return false;
             int column = (int)char.GetNumericValue(plmove[1]);
-            if (!(makeMove(row, column)))
+            if (!(makeMove(row, column, player1)))
                 return false;
             else
                 return true;
@@ -63,9 +55,9 @@ namespace Tic_Tac_Toe
 
         private int checkVerticals()
         {
-            for (int i = 0; i < 3; i++)
-                if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != 0)
-                    return i;
+            for(int i = 0; i < 3; i++)
+                    if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != 0)
+                        return i;
             return -1; // If there's no win, return -1.
         }
 
@@ -95,34 +87,12 @@ namespace Tic_Tac_Toe
                 Console.WriteLine($"{board[i, 0]} {board[i, 1]} {board[i, 2]}");
         }
 
-        private bool act()
+        private string getInput(ref StreamReader file, bool player1)
         {
-            if (inp == -1) // IF null reached.
-            {
-                if (won)
-                {
-                    displayBoard();
-                    won = false;
-                    return true;
-                }
-                displayBoard(); //Display the board.
-                return true;
-            }
-            else if (inp == 0)
-            {
-                displayBoard(); // Change it with json output.
-                board = new int[3, 3];
-                return false;
-            }
-            return false;
-        }
-
-        private string getInput()
-        {
-            line = file.ReadLine();
+            string line = file.ReadLine();
             if (line == null)
                 return "null";
-            while (!((int)char.GetNumericValue(line[1]) == 1 || (int)char.GetNumericValue(line[1]) == 2) || !(((int)line[4] > 64) && ((int)line[4] < 68)) || !((int)char.GetNumericValue(line[5]) > -1 && (int)char.GetNumericValue(line[5]) < 3))
+            while (!((int)char.GetNumericValue(line[1]) == 1 || (int)char.GetNumericValue(line[1]) == 2) || !(((int)line[4] > 64) && ((int)line[4] < 68)) || !((int)char.GetNumericValue(line[5]) > -1 && (int)char.GetNumericValue(line[5]) < 3) || (int)char.GetNumericValue(line[1]) != (player1 ? 1 : 2))
             {
                 if (line == null) // If end of file reached
                     return "null";
@@ -131,133 +101,34 @@ namespace Tic_Tac_Toe
                 line = file.ReadLine();
                 if (line == null) // If end of file reached
                     return "null";
-                if (firstmove)
-                {
-                    player1 = ((int)char.GetNumericValue(line[1]) == 1) ? true : false;
-                    firstmove = false;
                 }
-                else if((int)char.GetNumericValue(line[1]) != (player1 ? 1 : 2))
-                { 
-                    continue;
-                }
-            }
             // if this place is reached, that means input is legal. Return move.(does not mean that move is possible).
             return line.Substring(4, 2);
 
         }
-        private int makeSenseOfInput()
+        private int makeSenseOfInput(ref StreamReader file, ref bool player1)
         {
             string input;
             do
             {
-                input = getInput();
+                input = getInput(ref file, player1);
                 if (input == "00000000")
                 {
                     return 0;
-                } else if (input == "null")
+                }else if(input == "null")
                 {
                     return -1;
                 }
 
-            } while (!move(input)); // If move is not possible, repeat.
+            } while (!move(input, player1)); // If move is not possible, repeat.
             // IF this part of code is reached, this means that move is legal, and move was made.
             // This means, I need to invert the player.
             player1 = !player1;
             return 1;
         }
 
-        private bool firstMove() // Returns true if end reached.
-        {
-            if (makeSenseOfInput() == 0) // IF 000000 reached
+            public void gameLoop()
             {
-                if (skipMoves()) return true; // Skip moves and return NULL if end reached.
-            }
-            // determine who is first player.
-            //line = file.ReadLine();
-            player1 = ((int)char.GetNumericValue(line[1]) == 1) ? true : false; 
-            // Make Move;
-            move(line.Substring(4, 2));
-            player1 = !player1;
-            return false;
-        }
-        // If won or 0000000 reached, skip to the end of file or 
-        private bool skipMoves() {
-            do
-            {
-                line = file.ReadLine();
-                if (line == null)
-                    return true;
-            } while (line != "0000000000");
-            return false;
-        }
-        
-        private bool reInit()
-        {
-            displayBoard();
-            board = new int[3, 3];
-            if (skipMoves()) return true;
-            if (firstMove()) return true;
-            return false;
-        }
-        private bool playFromTextFile()
-        {
-            firstmove = true;
-            if (firstMove()) return true;
-            won = false;
-            while (true) // This is the game loop.
-            {
-                // Take input, if game is finished, return
-                do
-                {
-                    inp = makeSenseOfInput(); // Returns move if it is legal.
-                    if (act())                // If game is finished. Return.
-                        return true;
-                } while (inp != 1);
-
-                if (inp == 0) // if 00000000 reached
-                {
-                    if (reInit()) return true;
-                }
-                else if (checkWin() > -1)  // In case either player wins.
-                {
-                    won = true;
-                    if (reInit()) return true;
-                }
-            }
-        }
-
-        public void twoPlayerLoop()
-        {
-            Random r = new Random();
-            player1 = (r.Next(1, 2) == 1) ? true : false;//Choose player randomly.
-            string playerMove = "";
-            while (true)
-            {
-                Console.Clear();
-                Console.WriteLine($"Last Move: {playerMove}");//Display last move and board.
-                displayBoard();
-                Console.WriteLine($"PLayer {(player1 ? 1 : 2)}, enter a move:");//Promt for move.
-                playerMove = Console.ReadLine();
-
-                while (!(move(playerMove)))//While move  is illegal.
-                {
-                    Console.WriteLine("Invalid Move! Enter another move:"); // Ask for new move.
-                    playerMove = Console.ReadLine();
-                }
-                if (checkWin() > 0)
-                {
-                    Console.Clear();
-                    displayBoard();
-                    Console.WriteLine($"Player{(player1 ? 1 : 2)} won the game!");
-                    break;
-                }
-                player1 = !player1;
-
-            }
-
-        }
-        public void gameLoop()
-        {
             char gamemode;
             Console.WriteLine("NOTE: If you choose 2 player mode, 1st player will be chosen randomly.");
             Console.Write("If you want to use game.txt file, enter y, if you want 2 player game, enter n:");
@@ -265,20 +136,121 @@ namespace Tic_Tac_Toe
             Console.WriteLine();
             if (gamemode == 'y') // play using game.txt
             {
-                if (file != null)  
+                string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                path = Path.GetFullPath(Path.Combine(path, @"..\..\")) + "game.txt";
+                if (File.Exists(@path))  
                 {
-                    playFromTextFile();
+                    System.IO.StreamReader file = new System.IO.StreamReader(@path);
+                    string line;
+                    bool player1;
+                    line = file.ReadLine();                                             // Get first line.
+                    player1 = ((int)char.GetNumericValue(line[1]) == 1) ? true : false; // Determine first player.
+                    move(line.Substring(4, 2), player1);
+                    player1 = !player1;
+                    int inp;
+                    bool won = false;
+                    while (true)
+                    {
+
+                        do
+                        {
+                            inp = makeSenseOfInput(ref file, ref player1);
+                            if (inp == -1) // IF null reached.
+                            {
+                                if (won)
+                                {
+                                    displayBoard();
+                                    won = false;
+                                    goto finish;
+                                }
+                                displayBoard(); //Display the board.
+                                goto finish;
+                            }
+                            else if (inp == 0)
+                            {
+                                displayBoard(); // Change it with json output.
+                                board = new int[3, 3];
+                            }
+                        } while (inp != 1);
+                        if (inp == 0)
+                        {
+                            displayBoard();
+                            board = new int[3, 3];
+                            do
+                            {
+                                line = file.ReadLine();
+                                if (line == null)
+                                    goto finish;
+                            } while (line != "0000000000");
+                            line = file.ReadLine();
+                            /*if (line == null)
+                                goto finish;*/
+                            player1 = ((int)char.GetNumericValue(line[1]) == 1) ? true : false; // Determine first player.
+                            move(line.Substring(4, 2), player1); // Make Move
+                            player1 = !player1; // change Player
+                            continue;
+                        }
+                        if(checkWin() > -1)
+                        {
+                            displayBoard();
+                            board = new int[3, 3];
+                            won = true;
+                            // As game ends here, read new lines until null or 0000000 is reached.
+                            do
+                            {
+                                line = file.ReadLine();
+                                if (line == null)
+                                    goto finish;
+                            } while (line != "0000000000");
+                            line = file.ReadLine();
+                            /*if (line == null)
+                                goto finish;
+                            */
+                            player1 = ((int)char.GetNumericValue(line[1]) == 1) ? true : false; // Determine first player.
+                            move(line.Substring(4, 2), player1); // Make Move
+                            player1 = !player1; // change Player
+                            continue;
+                        }
+                    }
+                finish:
                     board = new int[3, 3];
                 }
                 else
                 {
-                    Console.WriteLine("please place game.txt (not game.txt.txt) in the folder with .cs files\n");
+                    Console.WriteLine($"{path} Does Not Exist!\n make sure text file is not named game.txt.txt instead of game.txt.\n");
                 }
             }
-            else //  This is 2 player case.
+            else // 
             {
-                twoPlayerLoop();
-            }                
+                Random r = new Random();
+                bool player1 = (r.Next(1, 2) == 1)? true:false;//Choose player randomly.
+                string playerMove = "";
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Last Move: {playerMove}");//Display last move and board.
+                    displayBoard();
+                    Console.WriteLine($"PLayer {(player1? 1:2)}, enter a move:");//Promt for move.
+                    playerMove = Console.ReadLine();
+
+                    while(!(move(playerMove, player1)))//While move  is illegal.
+                    {
+                        Console.WriteLine("Invalid Move! Enter another move:"); // Ask for new move.
+                        playerMove = Console.ReadLine();
+                    }
+                    if(checkWin() > 0)
+                    {
+                        Console.Clear();
+                        displayBoard();
+                        Console.WriteLine($"Player{(player1 ? 1 : 2)} won the game!");
+                        break;
+                    }
+                    player1 = !player1;
+
+                }
+            }
+                
+
         }
     }
 }
